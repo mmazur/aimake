f3485b72b702855b26ac559cfc20ded487a4bf3c
Mariusz Mazur <mmazur@redhat.com>
2025-01-12 11:48:50 +0100

    Sorat working `gen`

--- cmd/generate.go
+++ cmd/generate.go
@@ -6,22 +6,44 @@ Prompt:
 - Create an external command var called GenerateCmd.
 - Full name of the command is 'generate', but it also is available as an alias 'gen'.
 - By default this command takes one or more filenames. This is indicated when running --help as [FILES].
-  For each filename provided this way, a function called generateFile() is called with the filename as argument.
 - Alternatively it takes one argument: --all (alias -a), in which case no filenames are allowed.
-  In this case It scans the current directory and its subdirectories for files matching the "*.go" filename pattern and runs generateFile() against each.
+  In this case it scans the current directory and its subdirectories for files matching the "*.go" filename pattern.
 - If no arguments are provided, print help for the argument.
+- With the available file paths (whether provided or discovered), the following logic is executed:
+  pre, filePrompt, post, err = utils.GoParseFile(path)
+  prompt = promptPrefix + filePrompt
+  generateFile(path, pre, prompt, post)
 
-- generateFile() function performs the following actions:
-  Print the filename.
+Specifications:
+generateFile(path, pre, prompt, post) executes the following logic:
+  content = prompt + "\n\nCODE BLOCK START\n\n" + pre + post + "\n\nCODE BLOCK END"
+  use `content` as `prompt` for providers.QueryOpenAI()
+  If first line of response starts with ```, strip it.
+  If last line starts with ```, strip it as well.
+  Overwrite file given in `path` with prompt + '\n' + response.
 */
 
 import (
+	"aimake/providers"
+	"aimake/utils"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
+
+const promptPrefix = `
+I need to write computer code. Do not add any explanations before or after the code. Output only code.
+
+General requirements for me code are:
+Write code that is complete and directly runnable.
+DO NOT omit code or use comments such as "more content here" or "code remains unchanged."
+NEVER change anything above the line that contains the string "END IMMUTABLE CODE BLOCK"
+Do not unnecessarily remove any comments or code.
+
+The code itself must do the following:
+`
 
 // GenerateCmd represents the generate command
 var GenerateCmd = &cobra.Command{
@@ -40,7 +62,13 @@ var GenerateCmd = &cobra.Command{
 					return err
 				}
 				if filepath.Ext(path) == ".go" {
-					generateFile(path)
+					pre, filePrompt, post, err := utils.GoParseFile(path)
+					if err != nil {
+						fmt.Println("Error parsing file:", err)
+						return nil
+					}
+					prompt := promptPrefix + filePrompt
+					generateFile(path, pre, prompt, post)
 				}
 				return nil
 			})
@@ -51,7 +79,13 @@ var GenerateCmd = &cobra.Command{
 			cmd.Help()
 		} else {
 			for _, filename := range args {
-				generateFile(filename)
+				pre, filePrompt, post, err := utils.GoParseFile(filename)
+				if err != nil {
+					fmt.Println("Error parsing file:", err)
+					continue
+				}
+				prompt := promptPrefix + filePrompt
+				generateFile(filename, pre, prompt, post)
 			}
 		}
 	},
@@ -61,6 +95,12 @@ func init() {
 	GenerateCmd.Flags().BoolP("all", "a", false, "Generate all .go files in the current directory and subdirectories")
 }
 
-func generateFile(path string) {
-	fmt.Println(path)
+func generateFile(path, pre, prompt, post string) {
+	content := prompt + "\n\nCODE BLOCK START\n\n" + pre + post + "\n\nCODE BLOCK END"
+	response, err := providers.QueryOpenAI("gpt-4o-mini", content, "", "")
+	if err != nil {
+		fmt.Println("Error querying OpenAI:", err)
+		return
+	}
+	fmt.Println(response)
 }
